27a45f96cced3e350700445dbd80592fc98a84bd
Jose martins <josetabordamartins@gmail.com>
2018-08-12 16:43:02 +0100

    Changing anotation from @Service to @Component. Seems more logic

--- jumiacardservice/src/main/java/jumiacardservice/validators/CardNumberValidator.java
+++ jumiacardservice/src/main/java/jumiacardservice/validators/CardNumberValidator.java
@@ -1,11 +1,13 @@
 package jumiacardservice.validators;
 
 import org.apache.commons.validator.routines.CreditCardValidator;
+import org.springframework.stereotype.Component;
 import org.springframework.stereotype.Service;
 
 import jumiacardservice.exceptions.CardNumberValidationException;
 
-@Service
+
+@Component
 public class CardNumberValidator {
 
 	public boolean isValid(final String cardIn) {
